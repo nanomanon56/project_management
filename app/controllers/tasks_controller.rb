07628975d1cfefefeb8abc719ca765c8225bce71
nanomanon56 <manon.l.jean@gmail.com>
2017-12-21 11:54:30 -0500

    added delete function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,10 +2,20 @@ class TasksController < ApplicationController
   before_action :set_project
 
   def create
-    @task = @project.task.create(task_params)
+    @task = @project.tasks.create(task_params)
     redirect_to @project
   end
 
+  def destroy
+    @task = @project.tasks.find(params[:id])
+     if @tasks.destroy
+       flash[:success] = "Task was deleted."
+    else
+      flash[:error]="Task was not deleted"
+    end
+    redirect_to @project
+  end
+  
   private
   def set_project
   @project = Project.find(params[:project_id])
